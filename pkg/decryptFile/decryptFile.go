e1cbaaeb2657df7c7f0530f01387394c33297304
darshanags <darshanags@gmail.com>
2025-02-21 14:59:22 +0530

    adding support for goroutines for decrypt and encrypt functions

--- pkg/decryptFile/decryptFile.go
+++ pkg/decryptFile/decryptFile.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/darshanags/secure-files-go/pkg/appparser"
@@ -13,6 +14,16 @@ import (
 	"github.com/darshanags/secure-files-go/pkg/utilities"
 	"golang.org/x/crypto/chacha20poly1305"
 )
+
+type FileInfo struct {
+	InputFilename, InputPath, OutputFilename, OutputPath string
+}
+
+type DecryptFileAsyncResult struct {
+	Filename string
+	Message  string
+	Error    error
+}
 
 func DecryptFile(inputPath string, outputPath string, password string) (string, error) {
 
@@ -112,5 +123,139 @@ func DecryptFile(inputPath string, outputPath string, password string) (string, 
 	message = fmt.Sprintf("File decrypted. %s processed in %s.", utilities.FormatFileSize(float64(totalBytesRead)), time.Since(start))
 
 	return message, nil
+
+}
+
+func (file *FileInfo) DecryptFileAsync(password string, wg *sync.WaitGroup, resultChannel chan<- DecryptFileAsyncResult) {
+	defer wg.Done()
+	start := time.Now()
+
+	result := DecryptFileAsyncResult{
+		Filename: file.InputFilename,
+	}
+
+	encryptedFile, err := os.Open(file.InputPath)
+	if err != nil {
+		result.Error = fmt.Errorf("could not open the input file: %w", err)
+		resultChannel <- result
+		return
+	}
+	defer encryptedFile.Close()
+
+	fileStat, err := encryptedFile.Stat()
+	if err != nil {
+		result.Error = fmt.Errorf("could not get encrypted file information: %w", err)
+		resultChannel <- result
+		return
+	}
+	fileSize := fileStat.Size()
+
+	if fileSize < config.MinFileSize {
+		result.Error = fmt.Errorf("invalid file format: minimum file size mismatch")
+		resultChannel <- result
+		return
+	}
+
+	fileSignature, err := appparser.GetFileSignature(encryptedFile, "")
+	if err != nil {
+		result.Error = err
+		resultChannel <- result
+		return
+	}
+
+	if _, err := appparser.IsValidFileSignature(fileSignature); err != nil {
+		result.Error = err
+		resultChannel <- result
+		return
+	}
+
+	nonce := make([]byte, config.NonceSize)
+	if _, err := encryptedFile.Read(nonce); err != nil {
+		result.Error = fmt.Errorf("could not read the nonce: %w", err)
+		resultChannel <- result
+		return
+	}
+
+	salt := make([]byte, config.SaltSize)
+	if _, err := encryptedFile.Read(salt); err != nil {
+		result.Error = fmt.Errorf("could not read the salt: %w", err)
+		resultChannel <- result
+		return
+	}
+
+	encryptedDataEncKey := make([]byte, config.EncryptedDataEncKeySize+config.AuthTagSize)
+	if _, err := encryptedFile.Read(encryptedDataEncKey); err != nil {
+		result.Error = fmt.Errorf("could not read the secure encryption key: %w", err)
+		resultChannel <- result
+		return
+	}
+
+	_, derivedKey := kdf.Kdf(password, salt)
+
+	dataEncKeyCipher, err := chacha20poly1305.New(derivedKey)
+	if err != nil {
+		result.Error = fmt.Errorf("could not create key decryption cipher: %w", err)
+		resultChannel <- result
+		return
+	}
+
+	decryptedDataEncKey, err := dataEncKeyCipher.Open(nil, nonce, encryptedDataEncKey, nil)
+	if err != nil {
+		result.Error = fmt.Errorf("%w - could not decrypt the data encryption key, either the password is incorrect or the encrypted file has been altered", err)
+		resultChannel <- result
+		return
+	}
+
+	outputFile, err := os.OpenFile(file.OutputPath, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0755)
+	if err != nil {
+		result.Error = fmt.Errorf("failed to create output file: %w", err)
+		resultChannel <- result
+		return
+	}
+	defer outputFile.Close()
+
+	dataDecryptionCipher, err := chacha20poly1305.New(decryptedDataEncKey)
+	if err != nil {
+		result.Error = fmt.Errorf("could not create data decryption cipher: %w", err)
+		resultChannel <- result
+		return
+	}
+
+	chunk := make([]byte, config.ChunkSize+dataDecryptionCipher.Overhead())
+	chunkIndex := uint64(0)
+	var totalBytesRead uint64 = 0
+
+	for {
+		bytesRead, err := encryptedFile.Read(chunk)
+		if err != nil && err != io.EOF {
+			result.Error = fmt.Errorf("failed to read chunk: %w", err)
+			resultChannel <- result
+			return
+		}
+		if bytesRead == 0 {
+			break
+		}
+
+		decryptedChunk, err := dataDecryptionCipher.Open(nil, nonce, chunk[:bytesRead], nil)
+		if err != nil {
+			result.Error = fmt.Errorf("failed to decrypt chunk: %w", err)
+			resultChannel <- result
+			return
+		}
+
+		if _, err := outputFile.Write(decryptedChunk); err != nil {
+			result.Error = fmt.Errorf("failed to write decrypted chunk: %w", err)
+			resultChannel <- result
+			return
+		}
+
+		chunkIndex++
+		binary.LittleEndian.PutUint64(nonce[4:], chunkIndex)
+		totalBytesRead += uint64(bytesRead)
+	}
+
+	resultChannel <- DecryptFileAsyncResult{
+		Message: fmt.Sprintf("File decrypted: %s. %s processed in %s.", file.InputFilename, utilities.FormatFileSize(float64(totalBytesRead)), time.Since(start)),
+	}
 
 }
--- pkg/encryptFile/encryptFile.go
+++ pkg/encryptFile/encryptFile.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"slices"
+	"sync"
 	"time"
 
 	"github.com/darshanags/secure-files-go/pkg/config"
@@ -13,6 +14,16 @@ import (
 	"github.com/darshanags/secure-files-go/pkg/utilities"
 	"golang.org/x/crypto/chacha20poly1305"
 )
+
+type FileInfo struct {
+	InputFilename, InputPath, OutputFilename, OutputPath string
+}
+
+type EncryptFileAsyncResult struct {
+	Filename string
+	Message  string
+	Error    error
+}
 
 func EncryptFile(inputPath string, outputPath string, derivedKey []byte, salt []byte) (string, error) {
 
@@ -45,17 +56,17 @@ func EncryptFile(inputPath string, outputPath string, derivedKey []byte, salt []
 	headerData := slices.Concat([]byte(config.FileSignature), nonce, salt, encDataEncKey)
 
 	if _, err := outFile.Write(headerData); err != nil {
-		_ = fmt.Errorf("failed to write header data: %w", err)
+		return message, fmt.Errorf("failed to write header data: %w", err)
 	}
 
 	fileDataCipher, err := chacha20poly1305.New(dataEncKey)
 	if err != nil {
-		_ = fmt.Errorf("failed to create cipher: %w", err)
+		return message, fmt.Errorf("failed to create cipher: %w", err)
 	}
 
 	chunk := make([]byte, config.ChunkSize)
 	chunkIndex := uint64(0)
-	var totalBytesRead uint = 0
+	var totalBytesRead uint64 = 0
 
 	for {
 		bytesRead, err := inputFile.Read(chunk)
@@ -74,10 +85,95 @@ func EncryptFile(inputPath string, outputPath string, derivedKey []byte, salt []
 
 		chunkIndex++
 		binary.LittleEndian.PutUint64(nonce[4:], chunkIndex)
-		totalBytesRead += uint(bytesRead)
+		totalBytesRead += uint64(bytesRead)
 	}
 
 	message = fmt.Sprintf("File encrypted. %s processed in %s.", utilities.FormatFileSize(float64(totalBytesRead)), time.Since(start))
 
 	return message, nil
+}
+
+func (file *FileInfo) EncryptFileAsync(derivedKey []byte, salt []byte, wg *sync.WaitGroup, resultChannel chan<- EncryptFileAsyncResult) {
+	defer wg.Done()
+	start := time.Now()
+
+	result := EncryptFileAsyncResult{
+		Filename: file.InputFilename,
+	}
+
+	inputFile, err := os.Open(file.InputPath)
+	if err != nil {
+		result.Error = fmt.Errorf("failed to open input file: %w", err)
+		resultChannel <- result
+		return
+	}
+	defer inputFile.Close()
+
+	outFile, err := os.OpenFile(file.OutputPath, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0755)
+	if err != nil {
+		result.Error = fmt.Errorf("failed to create output file: %w", err)
+		resultChannel <- result
+		defer outFile.Close()
+		return
+	}
+	defer outFile.Close()
+
+	dataEncKey := genrandkey.GenRandKey(chacha20poly1305.KeySize, "data encryption key")
+	nonce := genrandkey.GenRandKey(config.NonceSize, "nonce")
+
+	dataEncKeyCipher, err := chacha20poly1305.New(derivedKey)
+	if err != nil {
+		result.Error = fmt.Errorf("failed to create cipher: %w", err)
+		resultChannel <- result
+		return
+	}
+
+	encDataEncKey := dataEncKeyCipher.Seal(nil, nonce, dataEncKey, nil)
+
+	headerData := slices.Concat([]byte(config.FileSignature), nonce, salt, encDataEncKey)
+
+	if _, err := outFile.Write(headerData); err != nil {
+		result.Error = fmt.Errorf("failed to write header data: %w", err)
+		resultChannel <- result
+		return
+	}
+
+	fileDataCipher, err := chacha20poly1305.New(dataEncKey)
+	if err != nil {
+		result.Error = fmt.Errorf("failed to create cipher: %w", err)
+		resultChannel <- result
+		return
+	}
+
+	chunk := make([]byte, config.ChunkSize)
+	chunkIndex := uint64(0)
+	var totalBytesRead uint64 = 0
+
+	for {
+		bytesRead, err := inputFile.Read(chunk)
+		if err != nil && err != io.EOF {
+			result.Error = fmt.Errorf("failed to read chunk: %w", err)
+			resultChannel <- result
+			return
+		}
+		if bytesRead == 0 {
+			break
+		}
+
+		encryptedChunk := fileDataCipher.Seal(nil, nonce, chunk[:bytesRead], nil)
+
+		if _, err := outFile.Write(encryptedChunk); err != nil {
+			result.Error = fmt.Errorf("failed to write encrypted chunk: %w", err)
+			resultChannel <- result
+			return
+		}
+
+		chunkIndex++
+		binary.LittleEndian.PutUint64(nonce[4:], chunkIndex)
+		totalBytesRead += uint64(bytesRead)
+	}
+
+	resultChannel <- EncryptFileAsyncResult{
+		Message: fmt.Sprintf("File encrypted: %s. %s processed in %s.", file.InputFilename, utilities.FormatFileSize(float64(totalBytesRead)), time.Since(start)),
+	}
 }
